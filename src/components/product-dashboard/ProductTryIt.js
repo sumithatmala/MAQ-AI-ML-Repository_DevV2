9c0ecd00b60643d21e3a9b7eeaabeee6b4d162b5
sumithatmala <atmala.prasanth@gmail.com>
2023-12-13 17:08:06 +0530

    styling product dask board

--- src/components/product-dashboard/ProductTryIt.js
+++ src/components/product-dashboard/ProductTryIt.js
@@ -4,27 +4,27 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const ProductTryIt = () => {
   return (
-    <section class="ftco-section">
-    <Container fluid style={{paddingLeft: "15%", paddingRight: "15%"}}>
-        <Row className="bg-dark text-white p-4" >
+    <section className='product-information'>
+    <Container fluid>
+        <Row className="product-header  bg-voilet-linear-gradient text-white p-4" >
         <Col>
           <h1 className="display-4">Scikit-Learn</h1>
           <p className="lead">Powerful machine learning library for Python</p>
         </Col>
       </Row>
-      <Row style={{paddingLeft: "15%", paddingTop: "3%", paddingRight: "15%"}}>
+      <Row className='product-overview'>
         <Col>
             <h2>Scikit-Learn Tool Showcase</h2>
         </Col>
       </Row>
-      <Row style={{paddingLeft: "15%", paddingTop: "3%", paddingRight: "15%"}}>
+      <Row className='product-content'>
         <Col>
         <MDBContainer>
             <iframe
                 title="Scikit-Learn Tool"
-                width="800"
+                width="100%"
                 height="600"
-                src="https://example.com/scikit-learn-tool"
+                src="https://onecompiler.com/python/3wz7uns6h"
                 frameborder="0"
                 allowfullscreen
             ></iframe>
