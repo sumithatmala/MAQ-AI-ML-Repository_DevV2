ff8b2e3c6bec67a180d02505cd844ce23c691a9b
Vushil2002 <202001054@daiict.ac.in>
2024-01-29 18:12:38 +0530

    Added all page contents in the Text Analysis page

--- src/components/solutions/TextAnalysis.js
+++ src/components/solutions/TextAnalysis.js
@@ -1,12 +1,133 @@
 import headerImg from "./visuals/text-analysis.svg"
 import "./TextAnalysis.css"
-const TextAnalysis = () => {
+import { Link } from "react-router-dom";
+import { learnMoreItems, Banners, SpecialCards } from "./visuals/LearnMoreList";
+
+const ContactForm = () => {
+    const form = document.querySelector("form");
+    const name = document.getElementById("name");
+    const CompName = document.querySelector("compName");
+    const Email = document.querySelector("email");
+    const msg = document.querySelector("message");
+
+    return (
+        <div className="FormBox">
+            <section className="Contact">
+                <h2>Get started with InData Labs today.</h2>
+                <p>Let's talk about your text analysis project</p>
+                <form action="#">
+                    <div className="input-box">
+                        <div className="input-field field">
+                            <input type="text" placeholder="Name*" id="name" className="item" autoComplete="off"></input>
+                            <div className="error-txt">Full Name can't be blank</div>
+                        </div>
+                        <div className="input-field field">    
+                            <input type="text" placeholder="Company Name*" id="compName" className="item" autoComplete="off"></input>
+                            <div className="error-txt">Company Name can't be blank</div>
+                        </div>
+                    </div>
+                    <div className="input-box">
+                        <div className="input-field field">
+                            <input type="text" placeholder="Email Adress*" id="email" className="item" autoComplete="off"></input>
+                            <div className="error-txt">Email Adress can't be blank</div>
+                        </div>
+                        
+                        <div className="input-field field"> 
+                            <input type="text" placeholder="Phone" id="Phone" className="item" autoComplete="off"></input>
+                            <div className="error-txt">Error message</div>
+                        </div>
+                    </div>
+                    <div className="textarea-field field">
+                        <textarea name="" id="message" cols={30} rows={7} placeholder="Your Message" className="item" autoComplete="off"></textarea>
+                        <div className="error-txt">Error message</div>
+                    </div>
+                    <button type="submit">Send Message</button>
+                </form>
+            </section>
+        </div>
+    )
+}
+
+const SpecialContainer = ({SpecialCards}) => {
+    return (
+        <div className="SpecialContainer">
+            <h2>Why Work With Us</h2>
+            <p>We make it easy to build awesome text analysis software</p>
+            <div className="SpecialCards">
+                {/* <ul> */}
+                    {SpecialCards.map((item) => {
+                        return(
+                            <div className="indCard">
+                                <img src={process.env.PUBLIC_URL + item.img} alt={item.topic} height={80}></img>
+                                <h3>{item.topic}</h3>
+                                <p>{item.disc}</p>
+                            </div>
+                        )
+                    })} 
+                {/* </ul> */}
+            </div>
+        </div>
+    )
+}
+
+const Banner = ({banners}) => {
     return (
         <>
-            <div className="Headervisual">
+            {banners.map((item,count) => {
+                // console.log(item.heading);
+                return (
+                    <div className={'BannerVisual' + (count%2 ? 'Rgt' : 'Lft')}>
+                        <img src= {process.env.PUBLIC_URL + item.img} alt= {item.heading} width={355}></img>
+                        <div className="BannerDetails">
+                            <h2>{item.heading}</h2>
+                            <p>{item.details}</p>
+                            <ul>
+                                {item.points.map((point) => {
+                                    return <li>{point}</li>
+                                })}
+                            </ul>
+                            <button className="ContactBtn"><Link to={'/ContactUs'}> Contact Us </Link></button>
+                        </div>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+const TextAnalysis = () => {
+    let count=0;
+    return (
+        <>
+            <div className="HeaderVisual">
                 <h1>AI-powered text analysis software</h1>
                 <h2>We use state-of-the-art natural language processing techniques to extract more value from your text data</h2>
-                <img src={headerImg} className="cropSvg"/>
+                <img src={headerImg} alt="Text analysis banner" className="cropSvg"/>
+            </div>
+            <div className="InfoVisual">
+                <div className="HeaderBox">
+                    <h1>Text analysis software built with your unique requirements in mind</h1>
+                    <p>We build custom text analysis software that helps you extract meaning from text data and speed up reporting and analysis. We use our proprietary natural language processing technology to accelerate project delivery for our customers. Our NLP solutions were designed to propel your customer-centric initiatives such as customer feedback analysis, customer support automation, information access, semantic search.</p>
+                </div>
+                <div className="LearnMoreList">
+                    <ul>
+                        {learnMoreItems.map((item, index) => {
+                            // console.log(item.label);
+                            return (
+                                <div className="LearnMoreCard">
+                                    <div>
+                                        <img src={process.env.PUBLIC_URL + item.img} alt={index} width={25}></img>
+                                        <p>{item.disc}</p>
+                                        <Link to={item.link}>Learn More</Link>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <Banner banners = {Banners} count={count}/>
+                <SpecialContainer SpecialCards={SpecialCards}/>
+                <ContactForm/>
             </div>
         </>
     )
---
+++ src/components/solutions/visuals/LearnMoreList.js
@@ -0,0 +1,49 @@
+export const learnMoreItems = [
+    {label: "ChatGPT", disc: "Power your product with ChatGPT technology", img: "/icons/framework.svg", link: ""},
+    {label: "NLP-AI", disc: "NLP API for social media analytics", img: "/icons/ML.svg", link: ""},
+    {label: "NLP Models", disc: "Custom NLP models for customer feedback analysis", img: "/icons/nlp.svg", link: ""},
+]
+
+export const Banners = [
+    {
+        heading: "Power your product with NLP technology", 
+        details: "Increase your products’ customer engagement and satisfaction rates with the power of Natural Language Processing.", 
+        points: ["Semantic search","Speech recognition","Text categorization","Information extraction (from topic classification to entity extraction)"],
+        img:"/images/txtAnalysis/AI_power.svg",
+    },
+    {
+        heading: "NLP API for social media analytics",
+        details: "An easy-to-use Natural Language Processing API to analyze text data from social media and unlock rich audience insights including demographic information and even more:", 
+        points: ["Gender","Age","Location","Interests","Language", "etc."],
+        img:"/images/txtAnalysis/AI_power.svg",
+    },
+    {
+        heading: "Custom NLP models for customer feedback analysis", 
+        details: "Customer feedback analysis software powered by custom models that go beyond simple domain adoption. Perfect for companies whose customers and prospects use distinctive language.", 
+        points: ["Sentiment analysis","Topic detection","Sentiment scored topic extraction","Semantic ontology development"],
+        img:"/images/txtAnalysis/AI_power.svg",
+    },
+]
+
+export const SpecialCards = [
+    {
+        topic: "Customizable solutions",
+        disc: "We don’t settle for average. Our solutions are tailored to fit in with customers’ specific needs and the nature of their data.   ",
+        img: "/images/txtAnalysis/nlp-benefits-1.svg"
+    },
+    {
+        topic: "State of the art models",
+        disc: "We are always looking for new ways to improve our predictions and get the best possible results.",
+        img: "/images/txtAnalysis/nlp-benefits-2.svg"
+    },
+    {
+        topic: "Scalable results",
+        disc: "We plan for the future. Our solutions will grow with your business constantly increasing your capabilities.",
+        img: "/images/txtAnalysis/nlp-benefits-3.svg"
+    },
+    {
+        topic: "Highly professional team",
+        disc: "We are determined to pursue greatness in everything we do for our clients.",
+        img: "/images/txtAnalysis/nlp-benefits-4.svg"
+    },
+]
