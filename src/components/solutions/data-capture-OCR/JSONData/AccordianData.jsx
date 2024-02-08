2c64ae3fd2bcbcc410adccb177dde953d71ddde7
Vushil2002 <202001054@daiict.ac.in>
2024-02-08 16:50:32 +0530

    changed the static accordian data

--- src/components/solutions/data-capture-OCR/JSONData/AccordianData.jsx
+++ src/components/solutions/data-capture-OCR/JSONData/AccordianData.jsx
@@ -3,7 +3,11 @@ const AccordianData = [
         list: "Gaming",
         desc: {
             para:["Player metrics have always been the most valuable resource for game developers and analysts. They want to understand players, monitor and improve gameplay, test design hypotheses, and release products based on thoroughly gathered research."],
-            points: ["point1","point2","point3"],
+            points: [
+                "improve accuracy",
+                "reduce errors",
+                "eliminate manual tasks",
+            ],
         },
         
     },
@@ -11,7 +15,12 @@ const AccordianData = [
         list: "Education",
         desc: {
             para:["The educational sphere deals with huge amounts of paperwork every day.","Almost all crucial documents are in a paper format and it can be difficult to find necessary information quickly and store it in a convenient format. This makes it difficult and challenging for educational institutions to offer the best student experience."],
-            points: ["point1","point2","point3"]
+            points: [
+                "optimize educational process",
+                "eliminate manual data entry errors",
+                "improve the overall accuracy",
+                "improve the students experience",
+            ]
         },
         
     },
@@ -19,13 +28,21 @@ const AccordianData = [
         list: "Retail",
         desc: {
             para: ["The use of automated data capture services give you an overview of your customers' behavior.","Additionally, you can easily scan and extract data from various sources. This will give you some interesting insights and ways in which some areas can be optimized."],
-            points: ["point1","point2","point3"],
+            points: [
+                "reduce costs",
+                "improve customer services",
+                "increase the efficiency of store operation",
+            ],
         }
     },
     {
         list: "Travel",
             desc: {para: ["Big data can bring endless opportunities for the travel industry.","We help our clients collect and analyze valuable information from different sources at every stage of their customers' journey. We help them improve the customer experience and enhance the service provided."],
-            points: ["point1","point2","point3"]
+            points: [
+                "collect and structure valuable data",
+                "improve services provided",
+                "improve customer experience",
+            ]
         }
     },
 ]
