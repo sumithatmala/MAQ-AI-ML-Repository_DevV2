c3836c9cdfe0d7ebbc5559a3de2a845bc911eb33
Saurabh-Rangdale12 <prashantrangdale1234@gmail.com>
2024-02-15 16:50:52 +0530

    LLM page Insdustry section responsive

--- src/components/solutions/large-language-model/Industries.jsx
+++ src/components/solutions/large-language-model/Industries.jsx
@@ -5,79 +5,86 @@ import biMarketing from './img/bi-marketing.svg';
 import iconTelecommunications from './img/icon-telecommunications.svg';
 import biIconConslng from './img/bi-icon-conslng.svg';
 import iconSecurity from './img/icon-security.svg';
-import "./css/Industries.css";
+// import "./css/Industries.css";
 
-const Industries = () => {
-  return (
-    <section className="section-industries-alt">
-      <div className="industries__inner-alt __bi-impl__inner-alt">
-        <h2 className="head-block-center-alt">
-          Build a Custom LLM Model for Your Industry
-        </h2>
-        <div className="text-block-center-alt">
-          Our generative AI company delivers custom models rooted in AI
-          expertise &amp; years of cross-domain expertise.
-        </div>
-        <div className="industries__list-alt">
-          {industries.map((industry, index) => (
-            <div key={index} className="industries__item-link-alt">
-              <div className="industries__item-alt">
-                <div className="industries__item-icon-alt">
-                  <img src={industry.image} alt={industry.alt} />
-                </div>
-                <div className="industries__item-title-alt">{industry.title}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="block__action">
-          <a
-            href="large-language-model.html#section-contacts"
-            className="btn btn-empty"
-          >
-            Your Industry
-          </a>
-        </div>
-      </div>
-    </section>
-  );
-};
+// const Industries = () => {
+//   return (
+//     <section className="section-industries-alt">
+//       <div className="industries__inner-alt __bi-impl__inner-alt">
+//         <h2 className="head-block-center-alt">
+//           Build a Custom LLM Model for Your Industry
+//         </h2>
+//         <div className="text-block-center-alt">
+//           Our generative AI company delivers custom models rooted in AI
+//           expertise &amp; years of cross-domain expertise.
+//         </div>
+//         <div className="industries__list-alt">
+//           {industries.map((industry, index) => (
+//             <div key={index} className="industries__item-link-alt">
+//               <div className="industries__item-alt">
+//                 <div className="industries__item-icon-alt">
+//                   <img src={industry.image} alt={industry.alt} />
+//                 </div>
+//                 <div className="industries__item-title-alt">{industry.title}</div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//         <div className="block__action">
+//           <a
+//             href="large-language-model.html#section-contacts"
+//             className="btn btn-empty"
+//           >
+//             Your Industry
+//           </a>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-const industries = [
+const Industries = [
   {
+    iconSrc: <img src={biFinService} />,
     title: "FinTech",
-    image: biFinService,
+    // image: biFinService,
     alt: "Financial Services",
+    description: "",
   },
-  // {
-  //   title: "Sport & Wellness",
-  //   image: biFinService,
-  //   alt: "Sport & Wellness"
-  // },
   {
+    iconSrc: <img src={iconEcommerceAlt} />,
     title: "Retail & E-commerce",
-    image: iconEcommerceAlt,
+    // image: iconEcommerceAlt,
     alt: "ecommerce",
+    description: "",
   },
   {
+    iconSrc: <img src={biMarketing} />,
     title: "Marketing & Advertising",
-    image: biMarketing,
+    // image: biMarketing,
     alt: "Marketing & Advertising",
+    description: "",
   },
   {
+    iconSrc: <img src={iconTelecommunications} />,
     title: "Telecommunication",
-    image: iconTelecommunications,
+    // image: iconTelecommunications,
     alt: "telecommunications",
+    description: "",
   },
   {
+    iconSrc: <img src={biIconConslng} />,
     title: "Consulting services",
-    image: biIconConslng,
+    // image: biIconConslng,
     alt: "BI Consulting Services",
+    description: "",
   },
   {
+    iconSrc: <img src={iconSecurity} />,
     title: "Insurance",
-    image: iconSecurity,
+    // image: iconSecurity,
     alt: "security",
+    description: "",
   },
 ];
 
--- src/components/solutions/large-language-model/LargeLanguageModel.jsx
+++ src/components/solutions/large-language-model/LargeLanguageModel.jsx
@@ -3,7 +3,7 @@ import Banner from "../common/Banner";
 import Benfits from "./Benefits";
 import UseCases from "./UseCases";
 import Industries from "./Industries";
-import "./css/LargeLanguageModel.css"
+import "./css/LargeLanguageModel.css";
 import Expertise from "./Expertise";
 import Benfits2 from "./Benefits2";
 import Integration from "./Integration";
@@ -11,25 +11,36 @@ import OurTechStack from "./OurTechStack";
 import BroadExpertise from "./BroadExpertise";
 import CustomerSuccess from "../common/CustomerSuccess";
 import CutomerSuccessData from "./JSONData/CustomerSuccessData";
-import FAQs from '../../product-dashboard/FAQs'
+import FAQs from "../../product-dashboard/FAQs";
 import FAQsData from "./JSONData/FAQsData";
 import BannerData from "./JSONData/BannerData";
 import ContactForm from "../common/contactForm";
+import GridOfCards from "../common/GridOfCards";
 
 const LargeLanguageModel = () => {
   return (
     <div>
       <div>
-        <Banner BannerData={BannerData}/>
+        <Banner BannerData={BannerData} />
         <Benfits />
         <UseCases />
-        <Industries />
+        {/* <Industries /> */}
+        <div style={{width:"100%",display:"flex", justifyContent:"center", textAlign:"center"}}>
+        <div style={{maxWidth:"1126px", }}>
+        <GridOfCards
+          content={Industries}
+          items={{ item: Industries, NoCards: 3, title: "Build a Custom LLM Model for Your Industry", caption: "Our generative AI company delivers custom models rooted in AI expertise &amp; years of cross-domain expertise." }}
+        />
+        </div>
+        </div>
+
         <div class="quick-block">
           <h2 class="head-block-center w-800">
             Explore Our Advanced Language Model Solutions
           </h2>
           <div class="text-block-center">
-            Discover a diverse range of language model services tailored to your specific requirements.
+            Discover a diverse range of language model services tailored to your
+            specific requirements.
           </div>
         </div>
         <Expertise />
@@ -39,9 +50,13 @@ const LargeLanguageModel = () => {
         <OurTechStack />
         <BroadExpertise />
         {/* <WhyMAQSoftware /> */}
-        <CustomerSuccess items={CutomerSuccessData} heading="Customer Success" CardHeight="35rem"/>
-        <FAQs FAQs = {FAQsData} />
-        <ContactForm/>
+        <CustomerSuccess
+          items={CutomerSuccessData}
+          heading="Customer Success"
+          CardHeight="35rem"
+        />
+        <FAQs FAQs={FAQsData} />
+        <ContactForm />
       </div>
     </div>
   );
