40a20adddc09c021f6da1c2d21d2498dfcd77378
Vushil2002 <202001054@daiict.ac.in>
2024-02-19 15:25:56 +0530

    carousel styles changes

--- src/components/miscellaneous/Expertise/JSON/customerSuccessData.jsx
+++ src/components/miscellaneous/Expertise/JSON/customerSuccessData.jsx
@@ -9,37 +9,37 @@ const customerSuccessData = [
     {
         title: "Recipe Generator App using Artificial Inteligence ouhsaugug ofuiau",
         image: <img src={recipe} alt="" height={250}/>,
-        // description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
+        description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
         link: "#",
     },
     {
         title: "ChatGPT Prompt Engineering for Website Virtual Assistant",
         image: <img src={chat} alt=""/>,
-        // description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
+        description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
         link: "#",
     },
     {
         title: "Al Gender Analysis & Identification Toolkit",
         image: <img src={gender} alt=""/>,
-        // description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
+        description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
         link: "#",
     },
     {
         title: "Targeted Advertising System & API Built with AWS Cloud Environment",
         image: <img src={bigD} alt=""/>,
-        // description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
+        description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
         link: "#",
     },
     {
         title: "OCR Insurance Claims Processing Software",
         image: <img src={insurance} alt=""/>,
-        // description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
+        description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
         link: "#",
     },
     {
         title: "Investment Data Management Solution with AWS Cloud Environment",
         image: <img src={investment} alt=""/>,
-        // description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
+        description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sit quos totam corrupti iure hic recusandae incidunt iste quia magnam."],
         link: "#",
     },
 ]
--- src/components/solutions/common/CustomerSuccess.jsx
+++ src/components/solutions/common/CustomerSuccess.jsx
@@ -6,15 +6,15 @@ import "slick-carousel/slick/slick-theme.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./css/CustomerSuccess.css"
- 
- 
+
+
 const CustomerSuccess = (props) => {
   const { items, defaultSlidesToShow, CardHeight } = props;
- 
+
   const slidesToShow = props.slidesToShow || defaultSlidesToShow;
- 
+
   const settings = {
-   
+
     infinite: true,
     speed: 500,
     // passing the slides to show dynamically and default will be 3
@@ -40,63 +40,63 @@ const CustomerSuccess = (props) => {
       },
     ],
   };
- 
+
   useEffect(() => {
     AOS.init({
       delay: 20,
     });
     AOS.refresh();
   }, []);
- 
+
   useEffect(() => {
     // Update the CSS variable with the maximum height of cards
-    console.log(CardHeight)
-    document.documentElement.style.setProperty("--max-card-height", `${CardHeight? CardHeight: "50rem"}`);
+    // console.log(CardHeight)
+    document.documentElement.style.setProperty("--max-card-height", `${CardHeight ? CardHeight : "50rem"}`);
   }, []);
- 
+
   return (
     <section className="carousel_cost">
-    <section className="target-section" style={{ backgroundColor: "white" }}>
-      <Row
-        className="slick-list1 draggable fade-in product-overview d-flex justify-content-center "
-        style={{ backgroundColor: "white", height: "auto" }}
-      >
-        <h3 class="head-block-center">{props.heading}</h3>
-        <Slider {...settings}>
-          {items.map((outcome, index) => (
-            <div className="separate-slider__item" key={index}>
-              {outcome.image ? (
-                <div className="separate-slider__img-figure">
-                  {outcome.image}
-                </div>
-              ) : (
-                <></>
-              )}
-              <div className="separate-slider__content">
-                <h3 className="separate-slider__title">{outcome.title}</h3>
-                <div className="separate-slider__text">
-                  {outcome.description?(outcome.description.map((paragraph, index) => (
-                    <p key={index}> {paragraph} </p>
-                  ))):(<></>)}
-                </div>
-                {outcome.link ? (
-                  <a href={outcome.link}>Learn More</a>
+      <section className="target-section" style={{ backgroundColor: "white" }}>
+        <Row
+          className="slick-list1 draggable fade-in product-overview d-flex justify-content-center "
+          style={{ backgroundColor: "white", height: "auto" }}
+        >
+          <h3 class="head-block-center">{props.heading}</h3>
+          <Slider {...settings}>
+            {items.map((outcome, index) => (
+              <div className="separate-slider__item" key={index}>
+                {outcome.image ? (
+                  <div className="separate-slider__img-figure">
+                    {outcome.image}
+                  </div>
                 ) : (
                   <></>
                 )}
+                <div className="separate-slider__content">
+                  <h3 className="separate-slider__title">{outcome.title}</h3>
+                  <div className="separate-slider__text">
+                    {outcome.description ? (outcome.description.map((paragraph, index) => (
+                      <p key={index}> {paragraph} </p>
+                    ))) : (<></>)}
+                  </div>
+                  {outcome.link ? (
+                    <a href={outcome.link}>Learn More</a>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
-      </Row>
-    </section>
+            ))}
+          </Slider>
+        </Row>
+      </section>
     </section>
   );
 };
- 
+
 // Set a default value for slidesToShow if not provided
 CustomerSuccess.defaultProps = {
   defaultSlidesToShow: 3,
 };
- 
+
 export default CustomerSuccess;
