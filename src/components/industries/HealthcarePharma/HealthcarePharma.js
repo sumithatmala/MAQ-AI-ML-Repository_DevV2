4599e0653dbba67240af84ca33cee39288f3bd67
Saurabh-Rangdale12 <prashantrangdale1234@gmail.com>
2024-03-07 09:23:14 +0530

    accodion modifing

--- src/components/industries/HealthcarePharma/HealthcarePharma.js
+++ src/components/industries/HealthcarePharma/HealthcarePharma.js
@@ -14,6 +14,7 @@ import ServiceProvide from "./ServiceProvides";
 import BigData from "./BigDataPharma";
 import OneCardCoursal from "../common/OneCardCoursal";
 import OCDataHealth from "./CarouselDataHealth";
+import BiotechDigitalTransformation from "../../solutions/common/accordion2";
 
 const HealtcarePharma = () => {
   return (
@@ -41,6 +42,8 @@ const HealtcarePharma = () => {
           </div>
         </div>
       </section>
+
+      <BiotechDigitalTransformation />
 
       <div style={{ display: "flex", justifyContent: "center" }}>
         <AccordionStatic
