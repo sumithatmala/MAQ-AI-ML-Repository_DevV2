31e565c251827ae54e568ddb96ec308e1ecc9118
Saurabh-Rangdale12 <prashantrangdale1234@gmail.com>
2024-03-29 09:39:07 +0530

    banner height set for 1240

--- src/components/HomePage/Banner.js
+++ src/components/HomePage/Banner.js
@@ -1,55 +1,56 @@
 import React from "react";
 import "./Banner.css";
-import hero from "./img/homepage.png"
-// import heroTrial from "./images/Framebackgorund-trial.png"
-import mouseIcon from "./img/mouse-icon.png"
-import downArrow from "./img/down-arow.png"
+import hero from "./img/homepage.png";
+import mouseIcon from "./img/mouse-icon.png";
+import downArrow from "./img/down-arow.png";
 
 export default function Banner() {
   return (
     <>
-    <section id="hero" className="hero">
-      <img src={hero} style = {{marginTop: "0%"}} alt="" className="box fade-into-view hero-img"/>
-      <div className="container" style={{marginTop:"8%"}}>
-        <div className="col mx-5">
-          <div className="col-lg-5">
-            <h2 className="box fade-up1" style={{fontFamily:"segoe UI"}}>
-              Discover Our AI & ML Solutions
-            </h2>
-            <p className="box fade-up2 mt-1 " style={{fontFamily:"segoe UI"}}>
-            Explore our AI & ML solutions to experience how our advanced algorithms drive innovation and enhance productivity. See how we revolutionize industries with transformative technologies, reshaping the future of businesses.
-            </p>
+      <section id="hero" className="hero">
+        <img
+          src={hero}
+          style={{ marginTop: "0%" }}
+          alt=""
+          className="box fade-into-view hero-img"
+        />
+        <div className="container" style={{ marginTop: "8%" }}>
+          <div className="col mx-5">
+            <div className="col-lg-5">
+              <h2 className="box fade-up1" style={{ fontFamily: "segoe UI" }}>
+                Discover Our AI & ML Solutions
+              </h2>
+              <p
+                className="box fade-up2 mt-1 "
+                style={{ fontFamily: "segoe UI" }}
+              >
+                Explore our AI & ML solutions to experience how our advanced
+                algorithms drive innovation and enhance productivity. See how we
+                revolutionize industries with transformative technologies,
+                reshaping the future of businesses.
+              </p>
+            </div>
           </div>
-          {/* Reach out button */}
-          {/* <div className="reach-out mt-4 fade-up3 ml-3" style={{fontFamily:"segoe UI"}}
-            onClick={() => {
-              const element = document.getElementsByClassName("parent-middle-sections");
-              // console.log(element);
-              //scroll down to the middle-section class
-              element[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-
-            }}>
-              Read more
-          </div> */}
         </div>
-        {/* add mouse image to show user to scroll to learn more */}
-      </div>
-        <div className="mouse-container move-mouse fade-up3" style={{}}
+        <div
+          className="mouse-container move-mouse fade-up3"
+          style={{}}
           onClick={() => {
-            const element = document.getElementsByClassName("parent-middle-sections");
-            // console.log(element);
-            //scroll down to the middle-section class
-            element[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-
+            const element = document.getElementsByClassName(
+              "parent-middle-sections"
+            );
+            element[0].scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+              inline: "nearest",
+            });
           }}
         >
-          <img src={mouseIcon} alt="" className="mouse-icon" width={"20px"}/>
+          <img src={mouseIcon} alt="" className="mouse-icon" width={"20px"} />
           <p className="m-auto learn-text">Learn more</p>
-          <img src={downArrow} alt="" className="down-arrow" width={"15px"}/>
+          <img src={downArrow} alt="" className="down-arrow" width={"15px"} />
         </div>
-      
-    </section>
-    
-  </>
+      </section>
+    </>
   );
 }
