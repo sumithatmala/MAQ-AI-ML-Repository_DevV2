c13c5eeb9a22b3860e8bd06db5bc73ad17150c29
Saurabh-Rangdale12 <prashantrangdale1234@gmail.com>
2024-04-10 11:32:24 +0530

    accessible tabs responsive

--- src/components/solutions/common/LLM Accordion/AccessibleTabs.jsx
+++ src/components/solutions/common/LLM Accordion/AccessibleTabs.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./AccessibleTabs.css"; // Import CSS file if necessary
 import { HashLink } from "react-router-hash-link";
+import Accordion from "../../../industries/HealthcarePharma/CustomAccordion/AccordionReact";
 
 const Tab = ({ title, content, imageUrl, isAcitive }) => (
   <li className="tab">
     <article className="tab-content">
       <h3>{title}</h3>
       <div className="center_para">
-      <p>{content}</p>
+        <p>{content}</p>
       </div>
     </article>
     <div className="tab-image">{imageUrl}</div>
@@ -18,6 +19,20 @@ const Tabs = ({ tabsData, sectionName, subheading, foot, bttn }) => {
   // State to keep track of the active tab index
   const [activeTab, setActiveTab] = useState(0);
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 930);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 930);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   // Function to handle tab click
   const handleTabClick = (index) => {
     setActiveTab(index);
@@ -25,52 +40,65 @@ const Tabs = ({ tabsData, sectionName, subheading, foot, bttn }) => {
 
   return (
     <>
-      <div>
+      <div className="Accordion_tabs" style={{width:"100%"}}>
         <h2 className="head-block-center">{sectionName}</h2>
         <div className="text-block-center">{subheading} </div>
 
-        <section className="sect">
-          <div className="section_div">
-            <ul className="indexes">
-              {tabsData.map((tab, index) => (
-                <li
-                  key={index}
-                  onClick={() => handleTabClick(index)}
-                  // onMouseEnter={() => handleTabClick(index)}
-                  className={activeTab === index ? "active active_border" : ""}
-                >
-                  <img src={tab.icon} style={{width:"20px", marginRight:"2px"}} />{" "}
-                  <span className="text_underline">{tab.heading} </span>{" "}
-                </li>
-              ))}
-            </ul>
-            {/* <ul className="tabs"> */}
-            <div className="ultabs">
-              {tabsData.map((tab, index) => (
-                <ul
-                  key={index}
-                  className={`tabs ${activeTab === index ? "active" : ""}`}
-                >
-                  <Tab
-                    key={index}
-                    title={tab.title}
-                    content={tab.content}
-                    imageUrl={tab.image}
-                    isAcitive={activeTab === index}
-                  />
+        <div className="Accordion_tabs" style={{width:"100%"}}>
+          {isMobile ? (
+            <Accordion listItems={tabsData} />
+          ) : (
+            <section className="sect">
+              <div className="section_div">
+                <ul className="indexes">
+                  {tabsData.map((tab, index) => (
+                    <li
+                      key={index}
+                      onClick={() => handleTabClick(index)}
+                      // onMouseEnter={() => handleTabClick(index)}
+                      className={
+                        activeTab === index ? "active active_border" : ""
+                      }
+                    >
+                      <img
+                        src={tab.icon}
+                        style={{ width: "20px", marginRight: "2px" }}
+                      />{" "}
+                      <span className="text_underline">{tab.heading} </span>{" "}
+                    </li>
+                  ))}
                 </ul>
-                // </div>
-              ))}
-            </div>
-            {/* </ul> */}
-          </div>
-        </section>
+                {/* <ul className="tabs"> */}
+                <div className="ultabs">
+                  {tabsData.map((tab, index) => (
+                    <ul
+                      key={index}
+                      className={`tabs ${activeTab === index ? "active" : ""}`}
+                    >
+                      <Tab
+                        key={index}
+                        title={tab.title}
+                        content={tab.content}
+                        imageUrl={tab.image}
+                        isAcitive={activeTab === index}
+                      />
+                    </ul>
+                    // </div>
+                  ))}
+                </div>
+                {/* </ul> */}
+              </div>
+            </section>
+          )}
+        </div>
 
         <div className="text-block-center"> {foot} </div>
         {bttn === true && (
           <div className="block__action">
-          <HashLink to="#contact" className="btn btn-empty">Request Demo</HashLink>
-        </div>
+            <HashLink to="#contact" className="btn btn-empty">
+              Request Demo
+            </HashLink>
+          </div>
         )}
       </div>
     </>
--- src/components/solutions/large-language-model/useCasesData.jsx
+++ src/components/solutions/large-language-model/useCasesData.jsx
@@ -1,80 +1,181 @@
-import globe from '../large-language-model/img/icon-accordion-language.svg';
-import star from '../large-language-model/img/icon-accordion-star.svg';
-import text from '../large-language-model/img/icon-accordion-text.svg';
-import education from '../large-language-model/img/icon-accordion-education.svg';
-import writing from '../large-language-model/img/icon-accordion-writing.svg';
-import chat from '../large-language-model/img/icon-accordion-chat.svg';
-import mobile from '../large-language-model/img/icon-accordion-mobile.svg';
+import globe from "../large-language-model/img/icon-accordion-language.svg";
+import star from "../large-language-model/img/icon-accordion-star.svg";
+import text from "../large-language-model/img/icon-accordion-text.svg";
+import education from "../large-language-model/img/icon-accordion-education.svg";
+import writing from "../large-language-model/img/icon-accordion-writing.svg";
+import chat from "../large-language-model/img/icon-accordion-chat.svg";
+import mobile from "../large-language-model/img/icon-accordion-mobile.svg";
 
-import chatbots_and_virtual_assistants  from '../large-language-model/img/ai-medical-diagnosis.webp'
-import translation_and_language_services  from '../large-language-model/img/Legal Document Analysis.webp'
-import text_analysis  from '../large-language-model/img/Financial Forecasting.webp'
-import content_generation  from '../large-language-model/img/Customer Service Automation.jpg'
-import personalized_recommendations  from '../large-language-model/img/Environmental Monitoring.jpg'
-import educational_tools   from '../large-language-model/img/Personalized Education.webp'
-import codegen from '../large-language-model/img/Code Generation.webp';
-
+import chatbots_and_virtual_assistants from "../large-language-model/img/ai-medical-diagnosis.webp";
+import translation_and_language_services from "../large-language-model/img/Legal Document Analysis.webp";
+import text_analysis from "../large-language-model/img/Financial Forecasting.webp";
+import content_generation from "../large-language-model/img/Customer Service Automation.jpg";
+import personalized_recommendations from "../large-language-model/img/Environmental Monitoring.jpg";
+import educational_tools from "../large-language-model/img/Personalized Education.webp";
+import codegen from "../large-language-model/img/Code Generation.webp";
 
 const AccessibleTabsData = [
-    
-    {
-      heading: "Medical Diagnosis Assistance",
-      title: "Medical Diagnosis Assistance",
-      content: 'Assist healthcare professionals in diagnosing diseases, interpreting medical reports, and recommending treatment plans based on patient symptoms and medical history.',
-      // imageUrl: 'https://picsum.photos/id/345/1000/600',
-      icon: mobile,
-      image:<img width="736" height="512" src={chatbots_and_virtual_assistants}  alt="Medical Diagnosis Assistance" />
-    },
-    {
-      heading: "Legal Document Analysis",
-      title: "Legal Document Analysis",
-      content: 'Analyze legal documents, contracts, and case law to identify relevant information, extract key insights, and provide legal professionals with valuable research support.',
-      // imageUrl: 'https://picsum.photos/id/352/1000/600',
-      icon:chat,
-      image:<img width="736" height="512" src={translation_and_language_services} alt="Legal Document Analysis" />,
-    },
-    {
-      heading: "Financial Forecasting",
-      title: "Financial Forecasting",
-      content: 'Predict market trends, analyze financial data, and generate accurate forecasts for stock prices, investment opportunities, and economic indicators using advanced machine learning techniques.',
-      // // imageUrl: 'https://picsum.photos/id/352/1000/600',
-      icon:globe,
-      image:<img width="736" height="512" src={text_analysis} alt="Financial Forecasting" />,
-    },
-    {
-      heading: "Customer Service Automation",
-      title: "Customer Service Automation",
-      content: 'Automate customer support processes, respond to inquiries, and resolve issues efficiently using chatbots, virtual assistants, and natural language processing capabilities.',
-      // // imageUrl: 'https://picsum.photos/id/352/1000/600',
-      icon:star,
-      image:<img width="736" height="512" src={content_generation}  alt="Customer Service Automation" />,
-    },
-    {
-      heading: "Code Generation",
-      title: "Code Generation",
-      content: 'Assist software developers in writing code, generating scripts, and automating repetitive programming tasks to improve productivity and accelerate software development cycles.',
-      // // imageUrl: 'https://picsum.photos/id/352/1000/600',
-      icon:text,
-      image:<img width="736" height="512" src={codegen} alt="Code Generation" />,
-    },
-    {
-      heading: "Environmental Monitoring",
-      title: "Environmental Monitoring",
-      content: 'Monitor environmental conditions, analyze climate data, and predict natural disasters such as floods, wildfires, and hurricanes to facilitate disaster preparedness and response efforts.',
-      // // imageUrl: 'https://picsum.photos/id/352/1000/600',
-      icon:education,
-      image:<img width="736" height="512" src={personalized_recommendations} alt="Environmental Monitoring" />,
-    },
-    {
-      heading: "Personalized Education",
-      title: "Personalized Education",
-      content: 'Tailor educational content and learning experiences to individual students based on their learning styles, preferences, and academic performance to enhance engagement and improve learning outcomes.',
-      // imageUrl: 'https://picsum.photos/id/352/1000/600',
-      icon:writing,
-      image:<img width="736" height="512" src={educational_tools} alt="Personalized Education" />,
-    },
-  ]
+  {
+    heading: "Medical Diagnosis Assistance",
+    list: "Medical Diagnosis Assistance",
+    title: "Medical Diagnosis Assistance",
+    content:
+      "Assist healthcare professionals in diagnosing diseases, interpreting medical reports, and recommending treatment plans based on patient symptoms and medical history.",
 
-  export default AccessibleTabsData;
+    desc: {
+      para: [
+        "Assist healthcare professionals in diagnosing diseases, interpreting medical reports, and recommending treatment plans based on patient symptoms and medical history.",
+      ],
+      points: [],
+    },
+    // imageUrl: 'https://picsum.photos/id/345/1000/600',
+    icon: mobile,
+    image: (
+      <img
+        width="736"
+        height="512"
+        src={chatbots_and_virtual_assistants}
+        alt="Medical Diagnosis Assistance"
+      />
+    ),
+  },
+  {
+    heading: "Legal Document Analysis",
+    list: "Legal Document Analysis",
+    title: "Legal Document Analysis",
+    content:
+      "Analyze legal documents, contracts, and case law to identify relevant information, extract key insights, and provide legal professionals with valuable research support.",
 
-  
+    desc: {
+      para: [
+        "Analyze legal documents, contracts, and case law to identify relevant information, extract key insights, and provide legal professionals with valuable research support.",
+      ],
+      points: [],
+    },
+    // imageUrl: 'https://picsum.photos/id/352/1000/600',
+    icon: chat,
+    image: (
+      <img
+        width="736"
+        height="512"
+        src={translation_and_language_services}
+        alt="Legal Document Analysis"
+      />
+    ),
+  },
+  {
+    heading: "Financial Forecasting",
+    list: "Financial Forecasting",
+    title: "Financial Forecasting",
+    content:
+      "Predict market trends, analyze financial data, and generate accurate forecasts for stock prices, investment opportunities, and economic indicators using advanced machine learning techniques.",
+
+    desc: {
+      para: [
+        "Predict market trends, analyze financial data, and generate accurate forecasts for stock prices, investment opportunities, and economic indicators using advanced machine learning techniques.",
+      ],
+      points: [],
+    },
+    // // imageUrl: 'https://picsum.photos/id/352/1000/600',
+    icon: globe,
+    image: (
+      <img
+        width="736"
+        height="512"
+        src={text_analysis}
+        alt="Financial Forecasting"
+      />
+    ),
+  },
+  {
+    heading: "Customer Service Automation",
+    list: "Customer Service Automation",
+    title: "Customer Service Automation",
+    content:
+      "Automate customer support processes, respond to inquiries, and resolve issues efficiently using chatbots, virtual assistants, and natural language processing capabilities.",
+
+    desc: {
+      para: [
+        "Automate customer support processes, respond to inquiries, and resolve issues efficiently using chatbots, virtual assistants, and natural language processing capabilities.",
+      ],
+      points: [],
+    },
+    // // imageUrl: 'https://picsum.photos/id/352/1000/600',
+    icon: star,
+    image: (
+      <img
+        width="736"
+        height="512"
+        src={content_generation}
+        alt="Customer Service Automation"
+      />
+    ),
+  },
+  {
+    heading: "Code Generation",
+    list: "Code Generation",
+    title: "Code Generation",
+    content:
+      "Assist software developers in writing code, generating scripts, and automating repetitive programming tasks to improve productivity and accelerate software development cycles.",
+
+    desc: {
+      para: [
+        "Assist software developers in writing code, generating scripts, and automating repetitive programming tasks to improve productivity and accelerate software development cycles.",
+      ],
+      points: [],
+    },
+    // // imageUrl: 'https://picsum.photos/id/352/1000/600',
+    icon: text,
+    image: <img width="736" height="512" src={codegen} alt="Code Generation" />,
+  },
+  {
+    heading: "Environmental Monitoring",
+    list: "Environmental Monitoring",
+    title: "Environmental Monitoring",
+    content:
+      "Monitor environmental conditions, analyze climate data, and predict natural disasters such as floods, wildfires, and hurricanes to facilitate disaster preparedness and response efforts.",
+
+    desc: {
+      para: [
+        "Monitor environmental conditions, analyze climate data, and predict natural disasters such as floods, wildfires, and hurricanes to facilitate disaster preparedness and response efforts.",
+      ],
+      points: [],
+    },
+    // // imageUrl: 'https://picsum.photos/id/352/1000/600',
+    icon: education,
+    image: (
+      <img
+        width="736"
+        height="512"
+        src={personalized_recommendations}
+        alt="Environmental Monitoring"
+      />
+    ),
+  },
+  {
+    heading: "Personalized Education",
+    list: "Personalized Education",
+    title: "Personalized Education",
+    content:
+      "Tailor educational content and learning experiences to individual students based on their learning styles, preferences, and academic performance to enhance engagement and improve learning outcomes.",
+
+    desc: {
+      para: [
+        "Tailor educational content and learning experiences to individual students based on their learning styles, preferences, and academic performance to enhance engagement and improve learning outcomes.",
+      ],
+      points: [],
+    },
+    // imageUrl: 'https://picsum.photos/id/352/1000/600',
+    icon: writing,
+    image: (
+      <img
+        width="736"
+        height="512"
+        src={educational_tools}
+        alt="Personalized Education"
+      />
+    ),
+  },
+];
+
+export default AccessibleTabsData;
