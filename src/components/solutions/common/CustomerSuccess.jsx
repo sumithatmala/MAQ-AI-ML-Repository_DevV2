18f40e85d8368a090b34e26a311d50e3b5acf147
sumithatmala <atmala.prasanth@gmail.com>
2024-05-01 15:58:32 +0530

    remove read more link in customer success

--- src/components/solutions/common/CustomerSuccess.jsx
+++ src/components/solutions/common/CustomerSuccess.jsx
@@ -86,13 +86,13 @@ const CustomerSuccess = (props) => {
                       <p key={index}> {paragraph} </p>
                     ))) : (<></>)}
                   </div>
-                  {outcome.link ? (
+                  {/* {outcome.link ? (
                     <div className="learn-more">
                     <a className="learn-more-text" href={outcome.link}>Learn More</a>
                     </div>
                   ) : (
                     <></>
-                  )}
+                  )} */}
                 </div>
               </div>
             ))}
